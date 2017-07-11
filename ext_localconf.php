cea41701f9eea43c51c9b7ab233dfbd7e8dc1b65
Dirk Persky <d.persky@gutenberghaus.de>
2017-07-11 15:08:03 +0200

    Set CKEditor to full mode, while i tags was stripes on reedit

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_fontawesome/Configuration/TSConfig/rte.txt">');
+
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/Default.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['full'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/Full.yaml';
