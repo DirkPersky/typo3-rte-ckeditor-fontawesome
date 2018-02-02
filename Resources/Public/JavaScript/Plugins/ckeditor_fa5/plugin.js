4916e4f1d8bdbdf88280c19c81d284726b1abe05
Dirk Persky <d.persky@gutenberghaus.de>
2018-02-02 16:04:05 +0100

    FA5 startup

---
+++ Resources/Public/JavaScript/Plugins/ckeditor_fa5/plugin.js
@@ -0,0 +1,24 @@
+(function ($) {
+    CKEDITOR.dtd.$removeEmpty.span = 0;
+    CKEDITOR.dtd.$removeEmpty.em = 0;
+    CKEDITOR.dtd.$removeEmpty.i = 0;
+
+    CKEDITOR.plugins.add('ckeditor_fa5', {
+    icons: 'ckeditor-fa',
+    init: function (editor) {
+      editor.addCommand('ckeditor_fa5', new CKEDITOR.dialogCommand('ckeditorFaDialog', {
+        allowedContent: 'i(!fa)',
+      }));
+      editor.ui.addButton('ckeditor_fa5', {
+        label: 'Insert FontAwesome icon',
+        command: 'ckeditor_fa5',
+        toolbar: 'insert',
+        icon: this.path + 'icons/ckeditor-fa.png',
+      });
+      CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=8.7.8');
+      CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=8.7.8');
+
+      editor.addContentsCss('https://use.fontawesome.com/releases/v5.0.6/css/all.css');
+    }
+  });
+})(TYPO3.jQuery);
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,4 @@ defined('TYPO3_MODE') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/Default.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['full'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/Full.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fa5'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/Fa5.yaml';
