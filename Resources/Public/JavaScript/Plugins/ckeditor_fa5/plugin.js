72213ab93e83fefd0edc3a9892dbe655a7a9b886
DirkPersky <d.persky@gutenberghaus.de>
2018-02-07 09:31:14 +0100

    Version fix

--- Resources/Public/JavaScript/Plugins/ckeditor_fa5/plugin.js
+++ Resources/Public/JavaScript/Plugins/ckeditor_fa5/plugin.js
@@ -15,8 +15,8 @@
         toolbar: 'insert',
         icon: this.path + 'icons/ckeditor-fa.png',
       });
-      CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=8.8.0');
-      CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=8.8.0');
+      CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=9.1.0');
+      CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=9.1.0');
 
       editor.addContentsCss('https://use.fontawesome.com/releases/v5.0.6/css/all.css');
     }
