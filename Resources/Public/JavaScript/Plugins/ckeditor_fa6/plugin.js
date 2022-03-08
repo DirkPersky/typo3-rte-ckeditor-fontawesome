5180a690831d93ecc6e7f88a0da2ff589fa6c42d
Dirk Persky <dirk.persky@gmail.com>
2022-03-08 20:08:53 +0100

    #24 FA6 update

---
+++ Resources/Public/JavaScript/Plugins/ckeditor_fa6/plugin.js
@@ -0,0 +1,24 @@
+(function ($) {
+    CKEDITOR.dtd.$removeEmpty.span = 0;
+    CKEDITOR.dtd.$removeEmpty.em = 0;
+    CKEDITOR.dtd.$removeEmpty.i = 0;
+
+    CKEDITOR.plugins.add('ckeditor_fa', {
+        icons: 'ckeditor-fa',
+        init: function (editor) {
+            editor.addCommand('ckeditor_fa', new CKEDITOR.dialogCommand('ckeditorFaDialog', {
+                allowedContent: 'i(!fa-*)',
+            }));
+            editor.ui.addButton('ckeditor_fa', {
+                label: 'Insert FontAwesome icon',
+                command: 'ckeditor_fa',
+                toolbar: 'insert',
+                icon: this.path + 'icons/ckeditor-fa.png',
+            });
+            CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=10.2.0');
+            CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=10.2.0');
+
+            editor.addContentsCss('https://use.fontawesome.com/releases/v6.0.0/css/all.css');
+        }
+    });
+})(TYPO3.jQuery);
--- ext_localconf.php
+++ ext_localconf.php
@@ -9,10 +9,14 @@ defined('TYPO3_MODE') or die();
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['defaultFA4'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA4.yaml';
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fullFA4'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA4.yaml';
     // Fontawesome V5
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA5.yaml';
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['full'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA5.yaml';
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['minimal'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/MinimalFA5.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['defaultFA5'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA5.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fullFA5'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA5.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['minimalFA5'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/MinimalFA5.yaml';
     // Frontawesome V5-Pro
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fa5pro'] = 'fileadmin/rte_ckeditor_fontawesome/FA5Pro.yaml';
+    // Fontawesome V6
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA6.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['full'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA6.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['minimal'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/MinimalFA6.yaml';
 })();
 
