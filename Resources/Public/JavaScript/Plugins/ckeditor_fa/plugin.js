0149366eb84957f0910f9474036988b635385a21
Dirk Persky <dirk.persky@gmail.com>
2022-03-08 20:35:39 +0100

    #24 FA6 release

--- Resources/Public/JavaScript/Plugins/ckeditor_fa/plugin.js
+++ Resources/Public/JavaScript/Plugins/ckeditor_fa/plugin.js
@@ -15,8 +15,8 @@
             toolbar: 'insert',
             icon: this.path + 'icons/ckeditor-fa.png',
         });
-        CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=11.1.0');
-        CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=11.1.0');
+        CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=11.5.2');
+        CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=11.5.2');
 
         editor.addContentsCss('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     }
--- Resources/Public/JavaScript/Plugins/ckeditor_fa5/plugin.js
+++ Resources/Public/JavaScript/Plugins/ckeditor_fa5/plugin.js
@@ -15,8 +15,8 @@
             toolbar: 'insert',
             icon: this.path + 'icons/ckeditor-fa.png',
         });
-        CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=11.1.0');
-        CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=11.1.0');
+        CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditor-fa.js?v=11.5.2');
+        CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditor-fa.css?v=11.5.2');
 
         editor.addContentsCss('https://use.fontawesome.com/releases/v5.15.4/css/all.css');
     }
