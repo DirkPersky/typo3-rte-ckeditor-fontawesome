28851e0d0245d4fc94ee3aee314074136a276345
Dirk Persky <dirk.persky@gmail.com>
2022-03-15 14:43:20 +0100

    #24 drop Fontawesome 4 and clear editor CSS

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,9 +5,6 @@ defined('TYPO3_MODE') or die();
 (function () {
     // add TS Config
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_fontawesome/Configuration/TSConfig/rte.txt">');
-    // Fontawesome V4
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['defaultFA4'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA4.yaml';
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fullFA4'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA4.yaml';
     // Fontawesome V5
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['defaultFA5'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA5.yaml';
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fullFA5'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA5.yaml';
